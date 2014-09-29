99214bead68efead21549935ede02d9eb562159e
jyy <njujiangyy@gmail.com>
2014-09-29 14:24:35 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  14:24:35 up 13 min,  1 user,  load average: 0.16, 0.05, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd---sshd---sshd---bash---make---sh-+-git      |                                       `-pstree      `-udevd---2*[udevd]

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -68,6 +68,7 @@ static void cmd_r() {
 			printf("The program is already running. Restart the program? (y or n)");
 			fflush(stdout);
 			scanf("%c", &c);
+			scanf(" %c", &c);
 			switch(c) {
 				case 'y': goto restart_;
 				case 'n': return;
@@ -92,62 +93,81 @@ void print_reg(){
 	printf("edi		0x%x		%d\n",cpu.edi,cpu.edi);
 	printf("eip		0x%x		0x%x\n",cpu.eip,cpu.eip);
 }
+void cmd_si(char *p) {
+	p=strtok(NULL," ");
+	if(nemu_state == END) {
+		restart();
+	}
+	if (nemu_state != BREAK_1)	nemu_state = RUNNING;
+	if(p==NULL)	cpu_exec(1);
+	else cpu_exec(atoi(p));
+	if(nemu_state != END) { nemu_state = STOP; }
+}
+void cmd_info(char *p) {
+	p=strtok(NULL," ");
+	if(strcmp(p,"r")==0){
+		print_reg();
+	}
+	else if (strcmp(p, "b") == 0) {
+		print_breakpoint();
+	}
+}
+void cmd_x(char *p) {
+	p=strtok(NULL," ");
+	int t=atoi(p);
+	p=strtok(NULL," ");
+	int k=strtol(p,NULL,16);
+	int i;
+	for(i=0;i<t;++i){
+		printf("0x%x	0x%02x\n",k,swaddr_read(k,4));
+		k+=4;
+	}
+}
+void cmd_b(char *p) {
+	p = strtok(NULL, " ");
+	uint32_t expr = strtol(p+1, NULL, 16);
+	BP *t = new_bp();
+	t -> addr = expr;
+	t -> inst = swaddr_read(expr, 1);
+	swaddr_write(expr, 1, 0xcc);
+}
+void cmd_d(char *p) {
+	p = strtok(NULL, " ");
+	int n;
+	if (p == NULL)	n = 100;
+	else n = atoi(p);
+	del_breakpoint(n);
+}
+void cmd_p(char *p) {
+
+}
 void main_loop() {
 	char *cmd;
 	while(1) {
 		cmd = rl_gets();
 		char *p = strtok(cmd, " ");
-
 		if(p == NULL) { continue; }
-
 		if(strcmp(p, "c") == 0) { cmd_c(); }
 		else if(strcmp(p, "r") == 0) { cmd_r(); }
 		else if(strcmp(p, "q") == 0) { return; }
 		else if(strcmp(p,"si") == 0){
-				p=strtok(NULL," ");
-				if(nemu_state == END) {
-					restart();
-				}
-				if (nemu_state != BREAK_1)	nemu_state = RUNNING;
-				if(p==NULL)	cpu_exec(1);
-				else cpu_exec(atoi(p));
-				if(nemu_state != END) { nemu_state = STOP; }
+				cmd_si(p);
 			}
 		else if(strcmp(p,"info")==0){
-			p=strtok(NULL," ");
-			if(strcmp(p,"r")==0){
-				print_reg();
-			}
-			else if (strcmp(p, "b") == 0) {
-				print_breakpoint();
-			}
+			cmd_info(p);
 		}
 		/* TODO: Add more commands */
 		else if(strcmp(p,"x")==0){
-			p=strtok(NULL," ");
-			int t=atoi(p);
-			p=strtok(NULL," ");
-			int k=strtol(p,NULL,16);
-			int i;
-			for(i=0;i<t;++i){
-				printf("0x%x	0x%02x\n",k,swaddr_read(k,4));
-				k+=4;
-			}
+			cmd_x(p);
 		}
 		else if (strcmp(p, "b") == 0) {
-			p = strtok(NULL, " ");
-			uint32_t expr = strtol(p+1, NULL, 16);
-			BP *t = new_bp();
-			t -> addr = expr;
-			t -> inst = swaddr_read(expr, 1);
-			swaddr_write(expr, 1, 0xcc);
+			cmd_b(p);
 		}
 		else if (strcmp(p, "d") == 0) {
-			p = strtok(NULL, " ");
-			int n;
-			if (p == NULL)	n = 100;
-			else n = atoi(p);
-			del_breakpoint(n);
+			cmd_d(p);
+		}
+		else if (strcmp(p, "p") == 0) {
+			cmd_p(p);
 		}
 		else { printf("Unknown command '%s'\n", p); }
 	}
