a4d56ec7ab9105b07ec3e07482bb0ff5732249a6
jyy <njujiangyy@gmail.com>
2014-10-08 23:48:00 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  23:48:00 up  5:26,  3 users,  load average: 1.33, 1.16, 1.09 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash-+-bomb      |      |                    |-2*[make---sh-+-nemu]      |      |                    |              `-tee]      |      |                    `-2*[vim]      |      |-sshd---sshd---bash-+-7*[make---sh-+-nemu]      |      |                    |              `-tee]      |      |                    `-make---sh-+-git      |      |                                `-pstree      |      `-sshd---sshd---bash---vim      `-udevd---2*[udevd]

--- src/ui/breakpoint.c
+++ src/ui/breakpoint.c
@@ -34,7 +34,7 @@ bool change() {
 		bool f = 1;
 		if (t -> kind == 1) {
 			uint32_t tmp = expr(t -> s, &f);
-			printf("%u\n", tmp);//debug
+			printf("%u %u\n", tmp, t -> now);//debug
 			if (tmp != t -> now) {
 				can = 1;
 				printf("Hardware watchpoint %d: %s\n", t -> NO, t -> s);
