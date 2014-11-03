e12e337ebfbc2fcfaa061b09400ceaeea8773cd9
jyy <njujiangyy@gmail.com>
2014-11-04 00:38:29 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  00:38:29 up  3:07,  3 users,  load average: 1.08, 1.04, 1.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash-+-make---sh-+-nemu      |      |                    |           `-tee      |      |                    `-vim      |      |-sshd---sshd---bash-+-2*[make---sh-+-nemu]      |      |                    |              `-tee]      |      |                    `-vim      |      `-sshd---sshd---bash-+-2*[make---sh-+-nemu]      |                           |              `-tee]      |                           |-make---sh-+-git      |                           |           `-pstree      |                           `-vim      `-udevd---2*[udevd]

--- src/exec/control/jcc-template.h
+++ src/exec/control/jcc-template.h
@@ -7,11 +7,11 @@ make_helper(concat(je_r_, SUFFIX)) {
 		DATA_TYPE t = instr_fetch(eip + 1, DATA_BYTE);
 	    uint32_t x;
 		if (DATA_BYTE == 1) {
-			if (t >> 7 & 1)	x = t | 0xffffff00;
+			if ((t >> 7) & 1)	x = t | 0xffffff00;
 			else x = t & 0xff; 
 		}
 		else if (DATA_BYTE == 2) {
-			if (t >> 15 & 1)	x = t | 0xffff0000;
+			if ((t >> 15) & 1)	x = t | 0xffff0000;
 			else x = t & 0xffff;
 		}
 		else x = t;
@@ -25,11 +25,11 @@ make_helper(concat(jmp_r_, SUFFIX)) {
 	DATA_TYPE t = instr_fetch(eip + 1, DATA_BYTE);
 	uint32_t x;
 	if (DATA_BYTE == 1) {
-		if (t >> 7 & 1)	x = t | 0xffffff00;
+		if ((t >> 7) & 1)	x = t | 0xffffff00;
 		else x = t & 0xff; 
 	}
 	else if (DATA_BYTE == 2) {
-		if (t >> 15 & 1)	x = t | 0xffff0000;
+		if ((t >> 15) & 1)	x = t | 0xffff0000;
 		else x = t & 0xffff;
 	}
 	else x = t;
