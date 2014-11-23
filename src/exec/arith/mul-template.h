96de9f2a5beda8987908f327398deb527abda95d
jyy <njujiangyy@gmail.com>
2014-11-23 22:31:30 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  22:31:30 up  7:57,  2 users,  load average: 1.40, 1.12, 1.08 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash-+-3*[make---sh-+-nemu]      |      |                    |              `-tee]      |      |                    `-vim      |      |-sshd---sshd---bash-+-2*[make---sh-+-nemu]      |      |                    |              `-tee]      |      |                    `-make---sh-+-git      |      |                                `-pstree      |      `-sshd---sshd---sftp-server      `-udevd---2*[udevd]

--- src/exec/arith/mul-template.h
+++ src/exec/arith/mul-template.h
@@ -117,7 +117,6 @@ make_helper(concat(imul_ibrm2r_, SUFFIX)) {
 		DATA_TYPE imm = (DATA_TYPE_S)t;
 		concat(setflagimul_, SUFFIX)(REG(m.R_M), imm, 0);
 		REG(m.reg) = (DATA_TYPE_S)REG(m.R_M) * (DATA_TYPE_S)imm;
-		puts("Cai!");
 		if(m.reg == m.R_M)	print_asm("imul   $0x%x,%%%s", t, REG_NAME(m.reg));
 		else print_asm("imul   $0x%x,%%%s,%%%s", t, REG_NAME(m.R_M), REG_NAME(m.reg));
 		return 3;
