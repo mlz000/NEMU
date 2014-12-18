6a55d5d6079cca0bdff241971d162737a4cf3451
jyy <njujiangyy@gmail.com>
2014-12-18 22:53:21 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  22:53:21 up  6:30,  4 users,  load average: 1.22, 1.09, 1.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-3*[sshd---sshd---bash---vim]      |      `-sshd---sshd---bash-+-2*[make---sh-+-nemu]      |                           |              `-tee]      |                           `-make---sh-+-git      |                                       `-pstree      `-udevd---2*[udevd]

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -14,7 +14,7 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 	assert(len == 1 || len == 2 || len == 4);
-	dram_write(addr, len, data);
+	Dcache_write(addr, len, data);
 }
 
 uint32_t swaddr_read(swaddr_t addr, size_t len) {
