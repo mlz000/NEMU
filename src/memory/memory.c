ffd1d3b2d932afba8043a67c5e32650f5dae6b05
jyy <njujiangyy@gmail.com>
2014-12-18 22:44:55 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  22:44:55 up  6:22,  4 users,  load average: 1.08, 1.03, 1.01 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-3*[sshd---sshd---bash---vim]      |      `-sshd---sshd---bash-+-make---sh-+-nemu      |                           |           `-tee      |                           `-make---sh-+-git      |                                       `-pstree      `-udevd---2*[udevd]

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -9,12 +9,12 @@ uint32_t Dcache_read(hwaddr_t addr, size_t len);
 void Dcache_write(hwaddr_t addr, size_t len, uint32_t data);
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	assert(len == 1 || len == 2 || len == 4);
-	return dram_read(addr, len);
+	return Dcache_read(addr,len);
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 	assert(len == 1 || len == 2 || len == 4);
-	dram_write(addr, len, data);
+	Dcache_write(addr, len, data);
 }
 
 uint32_t swaddr_read(swaddr_t addr, size_t len) {
@@ -29,7 +29,7 @@ void swaddr_write(swaddr_t addr, size_t len, uint32_t data) {
 
 static uint32_t hwaddr_read_instr(hwaddr_t addr, size_t len) {
 	assert(len == 1 || len == 2 || len == 4);
-	return Dcache_read(addr, len);
+	return Icache_read(addr, len);
 }
 
 uint32_t instr_fetch(swaddr_t addr, size_t len) {
