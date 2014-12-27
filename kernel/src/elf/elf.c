3bc01a41ff15ca04fdda08f7a4fd9ae6ed1e8615
jyy <njujiangyy@gmail.com>
2014-12-27 12:05:22 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  12:05:22 up 6 min,  1 user,  load average: 0.24, 0.13, 0.06 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---sftp-server      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-pstree      `-udevd---2*[udevd]

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -9,15 +9,15 @@
 #endif
 
 #define STACK_SIZE (1 << 20)
-
+uint32_t mm_malloc(uint32_t x, int len);
 void ide_read(uint8_t *, uint32_t, uint32_t);
 void create_video_mapping();
 uint32_t get_ucr3();
 
 uint32_t loader() {
 	Elf32_Ehdr *elf;
-	Elf32_Phdr *ph = NULL;
-
+	Elf32_Phdr *ph = NULL, *eph;
+	uint32_t *a, j;
 #ifdef HAS_DEVICE
 	uint8_t buf[4096];
 	ide_read(buf, ELF_OFFSET_IN_DISK, 4096);
@@ -26,24 +26,29 @@ uint32_t loader() {
 	/* The ELF file is located at memory address 0 */
 	elf = (void *)0x0;
 #endif
-
+	ph = (void *)elf + elf -> e_phoff;
+	eph = ph + elf -> e_phnum;
 	/* Load each program segment */
-	uint32_t ph_size = elf->e_phentsize*elf->e_phnum;
-
-	for (ph = (void*)elf->e_phoff; ph != (void*)(elf->e_phoff+ph_size); ph++) {
+	//nemu_assert(0);
+	for(; ph < eph; ++ph) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
-			void* p = (void*)mm_malloc(ph->p_vaddr, ph->p_memsz);
-			memcpy(p, (void*)elf+ph->p_offset, ph->p_filesz);
+			uint32_t* VirtAddr = (void*)mm_malloc(ph -> p_vaddr, ph -> p_memsz);
+			if (ph -> p_filesz) {
+				for (j = 0; j < (ph -> p_filesz - 1) / 4; ++j)
+					VirtAddr[j] = ((uint32_t*)ph -> p_offset)[j];
+			}
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			
-			memset(p+ph->p_filesz, 0, ph->p_memsz-ph->p_filesz);
+			 
+			 for (a = (void*)VirtAddr + ph -> p_filesz, j = 0; j < ph -> p_memsz - ph -> p_filesz; ++j)	a[j] = 0;
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
 
+
+			/* Record the prgram break for future use. */
 			extern uint32_t brk;
 			uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
 			if(brk < new_brk) { brk = new_brk; }
