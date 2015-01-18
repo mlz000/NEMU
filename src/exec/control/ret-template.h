71170e6389bba2662d0e6b34e92cfcd6f924cbdc
jyy <njujiangyy@gmail.com>
2015-01-18 17:15:01 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  17:15:01 up  1:40,  3 users,  load average: 1.08, 1.03, 1.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---vim      |      |-sshd---sshd---bash-+-17*[make---sh-+-nemu]      |      |                    |               `-tee]      |      |                    `-vim      |      `-sshd---sshd---bash-+-make---sh-+-nemu      |                           |           `-tee      |                           `-make---sh-+-git      |                                       `-pstree      `-udevd---2*[udevd]

--- src/exec/control/ret-template.h
+++ src/exec/control/ret-template.h
@@ -23,23 +23,14 @@ make_helper(concat(reti_, SUFFIX)) {
 }
 //call
 make_helper(concat(call_r_, SUFFIX)) {
-	cpu.esp -= DATA_BYTE;
-	MEM_W(cpu.esp, cpu.eip + 1 + DATA_BYTE);
-	DATA_TYPE t = instr_fetch(eip + 1, DATA_BYTE);
-	uint32_t x;
-	if (DATA_BYTE == 1) {
-		if ((t >> 7) & 1)	x = t | 0xffffff00;
-		else x = t & 0xff; 
-	}
-	else if (DATA_BYTE == 2) {
-		if ((t >> 15) & 1)	x = t | 0xffff0000;
-		else x = t & 0xffff;
-	}
-	else x = t;
-	if (DATA_BYTE == 2)	cpu.eip = (cpu.eip + x) & 0xffff;
-	else cpu.eip += x;
-	print_asm("call   0x%x", cpu.eip + DATA_BYTE + 1);
-	return DATA_BYTE + 1;
+	DATA_TYPE imm = instr_fetch(eip + 1, DATA_BYTE);
+	int t = imm;
+	if(DATA_BYTE == 2 && (t >> 15))t |= 0xffff0000;
+	cpu.esp-=DATA_BYTE;
+	MEM_W(cpu.esp,cpu.eip + 1 + DATA_BYTE);
+	cpu.eip += t;
+	print_asm("call   %x",cpu.eip + 1+DATA_BYTE);
+	return 1 + DATA_BYTE;
 }
 make_helper(concat(call_rm_, SUFFIX)) {
 	cpu.esp -= DATA_BYTE;
@@ -47,8 +38,7 @@ make_helper(concat(call_rm_, SUFFIX)) {
 	m.val = instr_fetch(eip + 1, 1);
 	if (m.mod == 3) {
 		MEM_W(cpu.esp, cpu.eip + 2);
-		if (DATA_BYTE == 2)	cpu.eip = REG(m.R_M) & 0xffff;
-		else cpu.eip = REG(m.R_M);
+		cpu.eip = REG(m.R_M);
 		print_asm("call   %%%s", REG_NAME(m.R_M));
 		cpu.eip -= 2;
 		return 2;
@@ -57,8 +47,7 @@ make_helper(concat(call_rm_, SUFFIX)) {
 		swaddr_t addr;
 		int len = read_ModR_M(eip + 1, &addr);
 		MEM_W(cpu.esp, cpu.eip + len + 1);
-		if (DATA_BYTE == 2)	cpu.eip = MEM_R(addr) & 0xffff;
-		else cpu.eip = MEM_R(addr);
+		cpu.eip = MEM_R(addr);
 		print_asm("call    %s", ModR_M_asm);
 		cpu.eip -= len + 1;
 		return len + 1;
